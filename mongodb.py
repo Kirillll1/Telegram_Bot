ccfe87146212eb836b19a1da09d4768dbb18b3ab
Kirillll1 <chobotar.kyrylo@chnu.edu.ua>
2024-10-30 11:19:42 +0100

    test

--- mongodb.py
+++ mongodb.py
@@ -1,11 +1,9 @@
 import logging
 from pymongo import MongoClient
-
+from config import MONGO_URI
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-
-MONGO_URI = "mongodb+srv://chobotarkyrylo:ltx8ZOKkpsmwOjAW@cluster0.ofuyj.mongodb.net/my_store_db?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URI)
 db = client['my_store_db']
 customers_collection = db['customers']
